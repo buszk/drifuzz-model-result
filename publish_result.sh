3e1ec56f6a22af633e9eb74a11841a15d43d5e8f
Zekun Shen <17345356+buszk@users.noreply.github.com>
2021-05-02 18:56:53 +0000

    add scripts

---
+++ publish_result.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dir=$HOME/Workspace/git/drifuzz-panda
+gitlink=git@github.com:buszk/drifuzz-model-result.git
+
+for d in $dir/work/*; do
+    echo $d
+    if [ -f $d/search.sav ]; then 
+        mkdir -p ././$(basename $d)/out
+        cp $d/out/0 ././$(basename $d)/out
+        cp $d/*.sav ./$(basename $d)
+    fi
+done
+
+git add *
+git commit -m "new result"
+git push -f origin master
